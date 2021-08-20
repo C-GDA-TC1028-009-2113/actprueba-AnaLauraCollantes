d785e40f70e74e0168fbcd0304554893ecb3902a
AnaLauraCollantes <a01641150@itesm.mx>
2021-08-20 13:01:14 +0000

    envio1

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
+    num1 = int(input('Dame un número: '))
+    num2 = int(input('Dame un número: '))
+    Suma = num1+num2
+    Resta = num1-num2
+    Multiplicacion = num1*num2
+    print('Suma: ' + str(Suma))
+    print('Resta: ' + str(Resta))
+    print('Multiplicación: ' + str(Multiplicacion))
+
+
 
 
 
